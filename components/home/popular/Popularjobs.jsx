b15edef1ab41f463a2bc9fc4f810d76dcade8851
PeterZabila <petrozabila@gmail.com>
2023-04-05 17:51:03 +0300

    1st commit index, popular, welcome

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+import styles from './popularjobs.style';
+import { COLORS, SIZES } from './popularjobs.style';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+
+const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
+
+  return (
+    <View>
+      <View>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList 
+            data = {[1, 2, 3, 4]}
+            renderItem={({item}) => (
+              <PopularJobCard item={item}/>
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+      
+    </View>
+  )
+}
+
+export default Popularjobs
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/petro.jpg";
+
+export default {
+  profile,
+};
