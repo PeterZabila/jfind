ecfd2a8331fa0de0980c9ca19436d658206eadb6
PeterZabila <petrozabila@gmail.com>
2023-04-06 18:00:49 +0300

    minor changes jobsCard

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { checkImageURL } from '../../../../utils';
 
 import styles from './popularjobcard.style';
 
@@ -13,17 +14,23 @@ const PopularJobCard = ({ item, selectedJob, handlePress }) => {
       style={styles.logoContainer(selectedJob, item)}
     >
         <Image
-          source={{ uri: item. employer_logo }}
-          resizeMode='contain'
-          style={styles.logoImage}
+          source={{
+            uri: checkImageURL(item?.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
        />
     </TouchableOpacity>
     <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
 
     <View style={styles.infoContainer}>
-      <Text style={styles.jobName(selectedJob, item)}>
+      <Text 
+        style={styles.jobName(selectedJob, item)}
+        numberOfLines={1}
+      >
         {item.job_title}
       </Text>
+      <Text style={styles.location}>{item.job_country}</Text>
     </View>
    </TouchableOpacity> 
   )
